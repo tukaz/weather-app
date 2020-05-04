bc1ce2fcefa05255f664d055659c7111d6ab2ab4
Lucas Fiorini <lucas.fiorini@avature.net>
2020-05-04 18:15:50 -0300

    Updates

---
+++ precache-manifest.84e2935cf2dd38851debffc796f2b35c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "c926a62af008fe920b2c",
+    "url": "/weather-app/static/js/main.7af7f5a2.chunk.js"
+  },
+  {
+    "revision": "f454c8447a94e5f92911",
+    "url": "/weather-app/static/js/2.e94418c5.chunk.js"
+  },
+  {
+    "revision": "c926a62af008fe920b2c",
+    "url": "/weather-app/static/css/main.a19b9d84.chunk.css"
+  },
+  {
+    "revision": "f454c8447a94e5f92911",
+    "url": "/weather-app/static/css/2.4648bcb3.chunk.css"
+  },
+  {
+    "revision": "21946ebab4e083879de4ccb791515f91",
+    "url": "/weather-app/index.html"
+  }
+];
