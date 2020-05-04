23c7b736f323da0e21ac2b7e03c00f026a88f374
Lucas Fiorini <lucas.fiorini@avature.net>
2020-05-04 19:10:03 -0300

    dev

--- src/constants/api_weather.js
+++ src/constants/api_weather.js
@@ -1,3 +1,3 @@
 export const API_WEATHER_KEY = '55fdb077b9665f80b70e061e5be0d251';
-export const API_WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
-export const API_WEATHER_FORECAST_URL = 'http://api.openweathermap.org/data/2.5/forecast';
+export const API_WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_WEATHER_FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast';
